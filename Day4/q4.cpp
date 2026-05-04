0c50079232c390d2eb2eeaf1badfe2eea4e200cd
chiralisomani <somanichirali01@gmail.com>
2026-05-04 11:14:53 +0530

    added day4 q4

---
+++ Day4/q4.cpp
@@ -0,0 +1,25 @@
+//Write a program in C++ to print the following pattern using nested loops
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size: ";
+    cin >> n;
+
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            
+            if(i == 1 || i == n || j == 1 || j == n) {
+                cout << "* ";
+            } else {
+                cout << "  ";
+            }
+
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
