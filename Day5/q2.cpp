0f5de71b0b1b297c38dfde3e6e1e5a412c72c342
chiralisomani <somanichirali01@gmail.com>
2026-05-05 09:35:02 +0530

    added day5q2

---
+++ Day5/q2.cpp
@@ -0,0 +1,14 @@
+//write code for types of function 
+//1) without arguments, without return type
+#include <iostream>
+using namespace std;
+
+// function definition (no arguments, no return)
+void display() {
+    cout << "This is a function without arguments and without return type";
+}
+
+int main() {
+    display();   // function call
+    return 0;
+}
