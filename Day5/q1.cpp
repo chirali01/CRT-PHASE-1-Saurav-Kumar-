22e5e1cad83923b457368aaefd71fc9022f34f22
chiralisomani <somanichirali01@gmail.com>
2026-05-05 09:17:07 +0530

    added day5q1

---
+++ Day5/q1.cpp
@@ -0,0 +1,13 @@
+//print a message using a function
+
+#include <iostream>
+using namespace std;
+void printmsg()
+{
+    cout<<"HELLO WORLD!"<<endl;
+}
+int main()
+{
+    printmsg();
+    return 0;
+}
