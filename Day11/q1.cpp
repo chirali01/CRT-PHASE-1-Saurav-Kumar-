f0919e644c80b1d3bcd2a5deb2093a3aac4d254f
chiralisomani <somanichirali01@gmail.com>
2026-05-11 10:03:55 +0530

    added day11q1

--- Day11/q1.cpp
+++ Day11/q1.cpp
@@ -1,6 +1,8 @@
-//WAP in c++ for linear search.
+//linear search
+
 #include <iostream>
 using namespace std;
+
 int LinearSearch(int nums[], int n, int target)
 {
     for (int i = 0; i < n; i++)
@@ -10,5 +12,26 @@ int LinearSearch(int nums[], int n, int target)
             return i;
         }
     }
-    return -1; //Element not found.
+
+    return -1;
+}
+
+int main()
+{
+    int nums[] = {10, 20, 30, 40, 50};
+    int n = 5;
+    int target = 30;
+
+    int result = LinearSearch(nums, n, target);
+
+    if (result != -1)
+    {
+        cout << "Element found at index: " << result;
+    }
+    else
+    {
+        cout << "Element not found";
+    }
+
+    return 0;
 }
